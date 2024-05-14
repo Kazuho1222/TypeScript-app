3e7a88669d80c82a64eaec1857246d08f09c9dbc
Kazuho1222 <destiny5914@gmail.com>
2024-05-15 08:42:26 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function MyButton({ title }: { title: string }) {
+  return (
+    <button>{title}</button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton title="I'm a button" />
+    </div>
+  );
+}
