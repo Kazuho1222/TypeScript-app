bdb41ad950c8ebd0fb68f3e7c81e2aaf20f07384
Kazuho1222 <destiny5914@gmail.com>
2024-05-16 00:22:01 +0900

    フック（useState）

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 
 interface MyButtonProps {
   title: string;
   disabled: boolean;
 }
+
+// Explicitly set the type to "boolean"
+const [enabled, setEnabled] = useState<boolean>(false);
+
+type Status = "idle" | "loading" | "success" | "error";
+
+const [Status, setStatus] = useState<Status>("idle");
+
+type RequestState =
+  | { status: "idle" }
+  | { status: "loading" }
+  | { status: "success", data: any }
+  | { status: "error", error: Error };
+
+const [requestState, setRequestState] = useState<RequestState>({ status: "idle" });
 
 function MyButton({ title, disabled }: MyButtonProps) {
   return (
